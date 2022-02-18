673d4a22919801c8bdd174ecb8d9bf434ed5a22e
Morten Skøtt <msk@siteimprove.com>
2022-02-18 11:30:29 +0100

    Rewrite to use types

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,53 @@ import (
 	"github.com/msk-siteimprove/conn-checker/pkg/conn"
 )
 
+type Persister interface {
+	FormatForCsv() []string
+}
+
+type OutputBase struct {
+	Id string
+	UnmodifiedInputUrl string
+}
+
+type ErrorOutputResult struct {
+	OutputBase
+	Error string
+}
+
+type SuccessOutputResult struct {
+	OutputBase
+	ConnectionResult *conn.ConnectionResult
+}
+
+func (r *ErrorOutputResult) FormatForCsv() []string {
+	return []string{}
+}
+
+func (r *SuccessOutputResult) FormatForCsv() []string {
+	return []string{}
+}
+
+func newErrorOutputResult(id, inputUrl, err string) Persister {
+	return &ErrorOutputResult{
+		OutputBase: OutputBase{
+			Id: id,
+			UnmodifiedInputUrl: inputUrl,
+		},
+		Error: err,
+	}
+}
+
+func newSuccessOutputResult(id, inputUrl string, connectionResult *conn.ConnectionResult) Persister {
+	return &SuccessOutputResult{
+		OutputBase: OutputBase{
+			Id: id,
+			UnmodifiedInputUrl: inputUrl,
+		},
+		ConnectionResult: connectionResult,
+	}
+}
+
 type Column uint16
 
 const (
@@ -20,16 +67,18 @@ const (
 	UrlCol Column = 29
 
 	// Input file
-	sites = "data/d09adf99-dc10-4349-8c53-27b1e5aa97b6.csv"
+	urlListInputPath string = "data/d09adf99-dc10-4349-8c53-27b1e5aa97b6.csv"
 
 	// Output files
-	outputSuccessPath = "output/success.csv"
-	outputFailedPath = "output/failed.csv"
+	outputSuccessPath string = "output/success.csv"
+	outputErrorPath string = "output/errors.csv"
+	// outputFailedPath = "output/failed.csv"
 )
 
 var (
-	outputSuccessData = make([]*conn.ConnectionResult, 10000) // Arbitrary estimated sizes
-	outputFailedData = make([]*conn.ConnectionResult, 5000)
+	outputSuccessData = make([]Persister, 10000) // Arbitrary estimated sizes
+	outputErrorData = make([]Persister, 10000) // Arbitrary estimated sizes
+	// outputFailedData = make([]*conn.ConnectionResult, 5000)
 )
 
 // Parse file
@@ -39,7 +88,7 @@ var (
 func main() {
 	fmt.Println("Conn-checker started")
 
-    f, err := os.Open(sites)
+    f, err := os.Open(urlListInputPath)
     if err != nil {
         log.Fatal("error while opening the file:", err)
     }
@@ -56,29 +105,36 @@ func main() {
             log.Fatalln("error while parsing input file entry:", err)
         }
 
-		parsedUrl, err := parseUrl(line[UrlCol])
+		// Columns read from csv
+		idEntry := line[IdCol]
+		urlEntry := line[UrlCol]
+
+		parsedUrl, err := parseUrl(urlEntry)
 		if err != nil {
+			fmt.Print("Appended parsedUrl error", parsedUrl)
+			outputErrorData = append(outputErrorData, newErrorOutputResult(idEntry, urlEntry, err.Error()))
 			log.Println("error while parsing to URL format:", err)
 			continue
 		}
 
-		// fmt.Println(parsedUrl.String())
-		// fmt.Println("before:", conformedUrl)
-		// fmt.Println("after:", parsedUrl.Host)
-
 		result, err := conn.Connect(parsedUrl.String())
 		if err != nil {
+			fmt.Print("Appended connect error", result)
+			outputErrorData = append(outputErrorData, newErrorOutputResult(idEntry, urlEntry, err.Error()))
 			log.Println("error while connecting to site:", parsedUrl.String(), err)
 			continue
 		}
 
-		if result.Status != 200 {
-			fmt.Println(result.Status)
+		if result.Status == 200 {
+			fmt.Println("Appended success", result)
+			outputSuccessData = append(outputSuccessData, newSuccessOutputResult(idEntry, urlEntry, result))
+		} else {
+			fmt.Println("Appended error", result)
+			outputErrorData = append(outputErrorData, newErrorOutputResult(idEntry, urlEntry, err.Error()))
 		}
-		// outputSuccessData = append(outputSuccessData, result)
 	}
-
 		// persist(outputSuccessData, outputSuccessPath)
+		persist(outputErrorPath, outputErrorData)
 }
 
 // Attempts to parse the given string into a URL.
@@ -113,6 +169,39 @@ func conform(url string) (string, error) {
 	return url, nil
 }
 
-func persist(data []*conn.ConnectionResult, relPath string) error {
+func prepare(persister []Persister, dataSize int) [][]string {
+	data := make([][]string, dataSize)
+	for _, p := range persister {
+		data = append(data, p.FormatForCsv())
+	}
+	return data
+}
+
+func persist(relPath string, ps []Persister) error {
+	data := [][]string{
+			{"vegetables", "fruits"},
+			{"carrot", "banana"},
+			{"potato", "strawberry"},
+		}
+
+    // create a file
+    file, err := os.Create(relPath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    // initialize csv writer
+    writer := csv.NewWriter(file)
+
+    defer writer.Flush()
+
+    // write all rows at once
+    writer.WriteAll(data)
+
+    // write single row
+    extraData := []string{"lettuce", "raspberry"}
+    writer.Write(extraData)
+
 	return nil
 }

fd54e727162db3f78177655a5f7b68e71bfbf47f
Morten Skøtt <msk@siteimprove.com>
2022-02-22 14:13:13 +0100

    Clean up robotstxt program a little

--- robotstxt/robots.py
+++ robotstxt/robots.py
@@ -9,22 +9,30 @@ def insert_into_csv_at(index: int, insert: str, into: str):
     modded_line = ','.join(map(str, lwords))
     return modded_line
 
+# Constants
 DEFAULT_USERAGENT_TOKENS = ["SiteimproveBot", "SiteimproveBot-Crawler"]
 ID_IDX = 0
 END_URL_IDX = 3
 STATUS_IDX = 4
 
-run_dir = "../runs/3_run/"
-successes = open(run_dir + "success.csv", "r")
-combined_output = open(run_dir + "success_robots.csv", 'a')
+# Paths
+run_dir = "../runs/4_run/"
+input_path = run_dir + "success.csv"
+output_path = run_dir + "success_robots.csv"
+robots_dir = run_dir + "robots/"
 parse_errors = []
 
-column_names = successes.readline() # Ignore column name row
-combined_output.write(insert_into_csv_at(STATUS_IDX+1, "Robots.txt OK", column_names))
+input_file = open(input_path, "r")
+output_file = open(output_path, 'a')
 
-print("Checking robots.txt of URL's from {0}...".format(successes))
+# Add entry to column names on first row
+column_names = input_file.readline()
+output_file.write(insert_into_csv_at(STATUS_IDX+1, "Robots.txt OK", column_names))
+
+print("Checking robots.txt of URL's from {0}...".format(input_file))
+print("Saving to {0}".format(output_path))
 while True:
-    line = successes.readline()
+    line = input_file.readline()
     if not line:
         break
 
@@ -33,22 +41,22 @@ while True:
     end_url_suc = entries_suc[END_URL_IDX]
 
     # Iterate over robot files using succes id
-    robots_txt_file = run_dir + "robots/" + id_suc + '.rob'
+    robots_txt_file = robots_dir + id_suc + '.rob'
     if exists(robots_txt_file):
         try:
             # Alternatively use ISO-8859-1 or Latin_1 is 8 bit character sets, so all garbage has a valid value. 
             with open(robots_txt_file, 'r', encoding='utf-8') as robotsfile:
                 contents = robotsfile.read() # Can potentially fail with Decoding Error
                 is_allowed = allowed_by_robots(contents, user_agents=DEFAULT_USERAGENT_TOKENS, url=end_url_suc)
-                combined_output.write(insert_into_csv_at(STATUS_IDX+1, str(is_allowed), line))
+                output_file.write(insert_into_csv_at(STATUS_IDX+1, str(is_allowed), line))
         except UnicodeDecodeError as e:
             error = "error: robots.txt with id {0} is not UTF-8 encoded: {1}".format(id_suc, e)
-            combined_output.write(insert_into_csv_at(STATUS_IDX+1, "N/A", line))
+            output_file.write(insert_into_csv_at(STATUS_IDX+1, "N/A", line))
             parse_errors.append(error)
             continue
 
-successes.close()
-combined_output.close()
+input_file.close()
+output_file.close()
 
 print("Parse errors encountered: {0}".format(len(parse_errors)))
 for error in parse_errors:

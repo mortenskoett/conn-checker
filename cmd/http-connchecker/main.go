32761e0e278eb722a66384f2e73207620df09c16
Morten Skøtt <msk@siteimprove.com>
2022-03-07 13:31:08 +0100

    WIP!!!! Begin rewriting to use json and HTTP error handling

--- cmd/http-connchecker/main.go
+++ cmd/http-connchecker/main.go
@@ -11,9 +11,14 @@ import (
 	"github.com/msk-siteimprove/conn-checker/pkg/work"
 )
 
+type ValidationResponse struct {
+	Success []work.JobResultSuccess `json:"validations"`
+	Errors []work.JobResultError `json:"errors"`
+}
+
 const (
 	port string = ":8080"
-	workerCount = 1
+	workerCount uint8 = 1
 )
 
 func main() {
@@ -28,34 +33,55 @@ func main() {
 }
 
 func validate(w http.ResponseWriter, r *http.Request) {
-	if contentType := r.Header.Get("Content-Type"); contentType != "" {
-			returnStatus(w, "Content-Type is not application/json", http.StatusUnsupportedMediaType)
-		}
-
 	var urls []work.UrlJob
 
-	err, status := decodeJsonBody(w, r, urls)
+	status, err := decodeJsonBodyInto(w, r, urls)
 	if err != nil {
 		returnStatus(w, err.Error(), status)
 	}
 
+	log.Println(urls)
+
 	// Create url job queue
 	var wg sync.WaitGroup
-	urlJobCh, successCh, errorsCh := work.PrepareJobQueues(workerCount, &wg)
+	jobQueue, successOut, errorsOut := work.PrepareJobQueue(workerCount, &wg)
 
-	// TODO Add data to work queue
+	// Add urls to queue
+	for _, url := range urls {
+		jobQueue <- url
+	}
 
 	// Wait for workers to finish processing urls
-	close(urlJobCh)
+	close(jobQueue)
 	wg.Wait()
-	// TODO Return successes and errors as json
+
+	response := ValidationResponse{
+		Success: make([]work.JobResultSuccess, 0, len(urls)/2), // TODO: Estimate
+		Errors: make([]work.JobResultError, 0, len(urls)/2),
+	}
+
+	// Return collected results
+	for success := range successOut {
+		response.Success = append(response.Success, success)
+	}
+	for err := range errorsOut {
+		response.Errors= append(response.Errors, err)
+	}
+
+	json, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	returnStatus(w, "pong", http.StatusOK)
 }
 
-func decodeJsonBody(w http.ResponseWriter, r *http.Request, outputVar interface{}) (error, int) {
+func decodeJsonBodyInto(w http.ResponseWriter, r *http.Request, outputVar interface{}) (int, error) {
+	if contentType := r.Header.Get("Content-Type"); contentType != "" {
+			returnStatus(w, "Content-Type is not application/json", http.StatusUnsupportedMediaType)
+		}
+
 	var syntaxError *json.SyntaxError
 	var unmarshalTypeError *json.UnmarshalTypeError
 
@@ -66,18 +92,18 @@ func decodeJsonBody(w http.ResponseWriter, r *http.Request, outputVar interface{
 	if err != nil {
 		if errors.As(err, &unmarshalTypeError) {
 			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			return errors.New(msg), http.StatusBadRequest
+			return  http.StatusBadRequest, errors.New(msg)
 
 		} else if errors.As(err, &syntaxError) {
 			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			return errors.New(msg), http.StatusBadRequest
+			return  http.StatusBadRequest, errors.New(msg)
 
 		} else {
-			msg := fmt.Sprintf("Bad request or internal server error", err) // TODO: Unsafe for public
-			return errors.New(msg), http.StatusBadRequest
+			msg := fmt.Sprintf("Bad request or internal server error: %s", err)
+			return  http.StatusBadRequest, errors.New(msg)
 		}
 	}
-	return nil, 0
+	return -1, nil
 }
 
 func returnStatus(w http.ResponseWriter, message string, status int) {
